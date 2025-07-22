01c682f2682fbd70c175198fa586eab9a2a2f091
tornike <tornikegerkena02@gmail.com>
2025-07-22 23:02:06 +0400

    added: missing file

---
+++ src/services/weapon.cpp
@@ -0,0 +1,34 @@
+#include <Arduino.h>
+#include <Servo.h>
+#include "weapon.h"
+
+Servo esc;
+const int escPin = 10;
+
+void weaponSetup() {
+  esc.attach(escPin, 1000, 2000);
+  esc.writeMicroseconds(1000);
+  delay(2000);
+  Serial.println("ESC Initialized");
+}
+
+void bldcSpeedControl(int percentage) {
+  int pulse;
+
+  switch (percentage) {
+    case 0: pulse = 1000; break;
+    case 25: pulse = 1250; break;
+    case 50: pulse = 1500; break;
+    case 75: pulse = 1750; break;
+    case 100: pulse = 2000; break;
+    default: pulse = 1000; Serial.println("Invalid BLDC percentage, stopping motor"); break;
+  }
+
+  esc.writeMicroseconds(pulse);
+  Serial.print("ESC set to ");
+  Serial.print(percentage);
+  Serial.print("% (Pulse: ");
+  Serial.print(pulse);
+  Serial.println("µs)");
+  delay(20);
+}
